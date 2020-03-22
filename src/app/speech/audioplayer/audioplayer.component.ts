14971b284126eefc09f892a84b34065e86242ef4
Rajeev M C <rajeevmc77@gmail.com>
2020-03-22 14:32:16 +0530

    added support for Audio files

--- src/app/speech/audioplayer/audioplayer.component.ts
+++ src/app/speech/audioplayer/audioplayer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-audioplayer',
@@ -6,8 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./audioplayer.component.css']
 })
 export class AudioplayerComponent implements OnInit {
+  private audioObj: any;
+  private currentURL: string;
 
-  constructor() { }
+  constructor() {
+    this.audioObj = new Audio();
+  }
+
+  public playURL(url: string) {
+    try {
+      this.audioObj.src = url;
+      this.currentURL = url;
+      this.audioObj.load();
+      this.audioObj.play();
+    } catch (exp) {
+      console.log(exp.message);
+    }
+  }
+
+  public playForDuration(from, to) {
+    try {
+      this.audioObj.src = this.currentURL + '#t=' + from + ',' + to;
+      this.audioObj.load();
+      this.audioObj.play();
+    } catch (exp) {
+      console.log(exp.message);
+    }
+  }
+
+  public stop() {
+    try {
+      this.audioObj.pause();
+      this.audioObj.currentTime = 0;
+    } catch (exp) {
+      console.log(exp.message);
+    }
+  }
 
   ngOnInit() {
   }
